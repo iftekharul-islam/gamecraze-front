080579e94f4dffa0b1eeeec6772c7d111024d00b
tushar-dev-aug <tushar@augnitive.com>
2020-06-17 11:08:05 +0600

    contacts form added

---
+++ src/components/Contacts.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <script type="application/javascript">
+            hbspt.forms.create({
+                css: '',
+                cssRequired: '',
+                portalId: "7914426",
+                formId: "94f62086-f74a-4d36-b32b-67a53916ecb9"
+            });
+        </script>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    fieldset {
+        max-width: 100% !important;
+    }
+</style>

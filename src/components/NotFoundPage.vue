01dc3456b619994bfdbd2d05f5e6f4b992a9a57d
iftekharul <iftekharul@augnitive.com>
2020-07-08 20:05:21 +0600

    404 Page added

---
+++ src/components/NotFoundPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <section class="page_404">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 ">
+                        <div class="col-sm-10 col-sm-offset-1  text-center">
+                            <div class="four_zero_four_bg">
+                                <h1 class="text-center ">404</h1>
+                            </div>
+                            <div class="contant_box_404">
+                                <h3 class="h2">
+                                    Look like you're lost
+                                </h3>
+                                <p>the page you are looking for not avaible!</p>
+                                <router-link to="/" class="btn btn-primary">Go to Home</router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFoundPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -22,6 +22,7 @@ import GameEdit from "../components/admin/GameEdit";
 // import Navbar from "../components/admin/partial/Navbar"
 import Password from "../components/login_registration/Password";
 import ForgotPassword from "../components/forgot_password/ForgotPassword";
+import NotFoundPage from "../components/NotFoundPage";
 
 
 let router = new Router({
@@ -142,8 +143,8 @@ let router = new Router({
         },
         {
             path: '*',
-            name: 'notFound',
-            component: GameList,
+            name: 'NotFoundPage',
+            component: NotFoundPage,
         },
     ]
 })

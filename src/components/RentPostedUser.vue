1dd6478f2bfe2cec119b6f40098ff1dc7c14d270
tushar-dev-aug <tushar@augnitive.com>
2020-08-05 11:16:33 +0600

    Issue solved

--- src/components/RentPostedUser.vue
+++ src/components/RentPostedUser.vue
@@ -2,46 +2,28 @@
   <div>
     <section class="cart sign-in-bg pt-5">
       <div class="container-fluid game-page-width pb-5">
-        <h1 class="text-white text-center">Rent Posted Users</h1>
+        <h1 class="text-white text-center">Available Right Now!</h1>
         <div class="w-50 m-auto pt-3">
           <div class="table-responsive pb-5">
             <table class="table table-striped table-dark">
               <thead>
                 <tr>
-                  <th scope="col">User Name</th>
+                  <th scope="col">Game Owner</th>
                   <th scope="col">Rating</th>
                   <th scope="col">Available From</th>
-                  <th scope="col">Max Rent Week</th>
+                  <th scope="col">Available For</th>
                 </tr>
               </thead>
               <tbody>
                 <tr v-for="(rent, index) in rents" :key="index">
-                  <th scope="row"> <router-link :to="{ path: '/rent-details/' + rent.id}">{{ rent.user.data.name }}</router-link></th>
+                  <th scope="row" class="h5"> <router-link :to="{ path: '/rent-details/' + rent.id}">{{ rent.user.data.name }}</router-link></th>
                   <td>#</td>
                   <td>{{ formattedDate(rent.availability_from_date) }}</td>
-                  <td>{{ rent.max_number_of_week }}</td>
+                  <td>{{ rent.max_number_of_week }} week(s)</td>
                 </tr>
               </tbody>
             </table>
           </div>
-<!--          <table class="table table-bordered text-white">-->
-<!--            <thead>-->
-<!--            <tr>-->
-<!--              <th scope="col">User Name</th>-->
-<!--              <th scope="col">Rating</th>-->
-<!--              <th scope="col">Available From</th>-->
-<!--              <th scope="col">Max Rent Week</th>-->
-<!--            </tr>-->
-<!--            </thead>-->
-<!--            <tbody>-->
-<!--            <tr v-for="(rent, index) in rents" :key="index">-->
-<!--              <th scope="row"> <router-link :to="{ path: '/rent-details/' + rent.id}">{{ rent.user.data.name }}</router-link></th>-->
-<!--              <td>#</td>-->
-<!--              <td>{{ formattedDate(rent.availability_from_date) }}</td>-->
-<!--              <td>{{ rent.max_number_of_week }}</td>-->
-<!--            </tr>-->
-<!--            </tbody>-->
-<!--          </table>-->
         </div>
       </div>
     </section>

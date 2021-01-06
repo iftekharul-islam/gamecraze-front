da846b4c449d7760573c27db2fa443bbc357938b
Debashish Roy <debashish@augnitive.com>
2021-01-06 17:47:32 +0600

    home top news, news list, news details fixed

--- src/components/Newsroom.vue
+++ src/components/Newsroom.vue
@@ -1,105 +1,45 @@
 <template>
     <div>
-       
-     
-    <!-- Newsroom Section -->
-    <section class="newsroom-section">
-        <div class="text-center newsroom-section--heading">
-                <h2>NEWSROOM</h2>
-            </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 mb-4">
-                    <div class="trending-news position-relative">
-                        <a href="#" class="w-100 h-100 d-flex align-items-end">
-                            <img src="../assets/img/newsroom.png" alt="newsroom" class="img-fluid w-100 position-absolute">
-                            <div class="trending-news--text-content">
-                                <p>Feature</p>
-                                <h2>Get PUBG Royal Pass free every month</h2>
-                                <p class="newsroom-date">January  15, 2021</p>
-                            </div>
-                        </a>
+        <section class="newsroom-section">
+            <div class="text-center newsroom-section--heading"> <h2>NEWSROOM</h2></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 mb-4">
+                        <div class="trending-news position-relative">
+                            <a href="#" class="w-100 h-100 d-flex align-items-end">
+                                <img src="../assets/img/newsroom.png" alt="newsroom" class="img-fluid w-100 position-absolute">
+                                <div class="trending-news--text-content">
+                                    <p>Feature</p>
+                                    <h2>Get PUBG Royal Pass free every month</h2>
+                                    <p class="newsroom-date">January  15, 2021</p>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- row -->
-            <div class="row">
-                <div class="col-md-6 mb-4">
-                    <a href="#" class="newsroom-section--content-box">
-                        <div class="newsroom-section--content-box--img">
-                            <img src="../assets/img/newsroom-box.png" alt="News box" class="w-100 img-fluid">
-                        </div>
-                        <div class="newsroom-section--content-box--text">
-                            <p>Update</p>
-                            <h6>Get PUBG Royal Pass free every month and Play like a Rockster</h6>
-                            <p>January  15, 2021</p>
-                        </div>
-                    </a>
+                <!-- row -->
+                <div class="row">
+                    <div v-for="(item, index) in news" :key="item.id" :class="{ 'col-md-6 mb-4': (index == 0 || index == 1) , 'col-md-4 mb-4' : (index != 0 || index != 1) }">
+                        <router-link :to="{ name: 'NewsStory', params: { id: item.id }}" class="newsroom-section--content-box">
+                            <div class="newsroom-section--content-box--img">
+                                <img :src=item.thumbnail :alt=item.title class="w-100 img-fluid">
+                            </div>
+                            <div class="newsroom-section--content-box--text">
+                                <p>Update</p>
+                                <h6 v-if="index == 0 || index == 1">{{ item.title && item.title.length > 53 ? item.title.substring(0, 53) + ".." : item.title }} </h6>
+                                <h6 v-else>{{ item.title && item.title.length > 28 ? item.title.substring(0, 28) + ".." : item.title }} </h6>
+
+                                
+                                <p>{{ item.created }}</p>
+                            </div>
+                        </router-link>
+                    </div>
                 </div>
-                 <div class="col-md-6 mb-4">
-                    <a href="#" class="newsroom-section--content-box">
-                        <div class="newsroom-section--content-box--img">
-                            <img src="../assets/img/newsroom-box.png" alt="News box" class="w-100 img-fluid">
-                        </div>
-                        <div class="newsroom-section--content-box--text">
-                            <p>Update</p>
-                            <h6>Get PUBG Royal Pass free every month and Play like a Rockster</h6>
-                            <p>January  15, 2021</p>
-                        </div>
-                    </a>
+                <div class="view-all-btn mt-5">
+                    <a href="#" class="btn--secondery m-auto"><span>VIEW ALL</span></a>
                 </div>
             </div>
-             <!-- row -->
-            <div class="row">
-                <div class="col-md-4 mb-4">
-                    <a href="#" class="newsroom-section--content-box">
-                       <div class="newsroom-section--content-box--img">
-                            <img src="../assets/img/newsroom-box.png" alt="News box" class="w-100 img-fluid">
-                        </div>
-                        <div class="newsroom-section--content-box--text">
-                            <p>Update</p>
-                            <h6>Get PUBG Royal Pass free every month and Play like a Rockster</h6>
-                            <p>January  15, 2021</p>
-                        </div>
-                    </a>
-                </div>
-                 <div class="col-md-4 mb-4">
-                    <a href="#" class="newsroom-section--content-box">
-                       <div class="newsroom-section--content-box--img">
-                            <img src="../assets/img/newsroom-box.png" alt="News box" class="w-100 img-fluid">
-                        </div>
-                        <div class="newsroom-section--content-box--text">
-                            <p>Update</p>
-                            <h6>Get PUBG Royal Pass free every month and Play like a Rockster</h6>
-                            <p>January  15, 2021</p>
-                        </div>
-                    </a>
-                </div>
-                 <div class="col-md-4 mb-4">
-                    <a href="#" class="newsroom-section--content-box">
-                        <div class="newsroom-section--content-box--img">
-                            <img src="../assets/img/newsroom-box.png" alt="News box" class="w-100 img-fluid">
-                        </div>
-                        <div class="newsroom-section--content-box--text">
-                            <p>Update</p>
-                            <h6>Get PUBG Royal Pass free every month and Play like a Rockster</h6>
-                            <p>January  15, 2021</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="view-all-btn mt-5">
-                <a href="#" class="btn--secondery m-auto"><span>VIEW ALL</span></a>
-            </div>
-        </div>
-    </section>
-
-    
-       
-
-
-
-
+        </section>
     </div>
 </template>
 
@@ -108,27 +48,25 @@
         data() {
             return {
                 user: {},
-                isToggle: false
+                isToggle: false,
+                news: []
             }
         },
         methods: {
-            formattedDate(date) {
-                const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-                let birthDate = new Date(date)
-                return birthDate.getDate() + " " + months[birthDate.getMonth()] + " " + birthDate.getFullYear()
+            getNews: function(perPage, order = 'DESC') {
+                this.$api.get('articles?perPage=' + perPage + '&order=' + order).then(response => {
+                    if (response.status == 200) {
+                        this.news = response.data.data;
+                        return;
+                    }
+
+                    this.$toaster.error('Something went wrong.');
+                    console.log('status: ', response.status);
+                });
             },
-            changeToggle() {
-                if (this.isToggle) {
-                    this.isToggle = false;
-                }
-                else {
-                    this.isToggle = true;
-                }
-            }
         },
         created() {
-            this.user = this.$store.state.user
-            console.log(this.user, 'user');
+            this.getNews(5, 'DESC');
         }
     }
 		

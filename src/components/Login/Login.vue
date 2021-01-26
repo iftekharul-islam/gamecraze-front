1d3204090bec0d228e759dd9340df64cd5ef8bec
Debashish Roy <debashish@augnitive.com>
2021-01-26 19:31:04 +0600

    prevent access to login page for auth user, make email field required

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -387,6 +387,9 @@
             }
         },
         created() {
+            if(this.$store.getters.ifAuthenticated) {
+                this.$router.push('/profile');
+            }
             this.$store.state.setPasswordPopUp = false;
             this.$root.$on('stopLoader', () => {
                 this.isLoading = false;
@@ -401,6 +404,7 @@
                 },
                 { deep:true }
             );
+            // console.log('auth', this.$store.getters.ifAuthenticated);
         },
         mounted() {
             document.body.classList.add('body-position')

8b6b967351881ea9c7b439887ef3b878a7950b49
iftekharul <iftekharul@augnitive.com>
2021-02-23 18:51:52 +0600

    #51 Notice Board Should be dynamic - story : https://trello.com/c/qKCULhgb/51-notice-board-should-be-dynamic

--- src/components/NoticeBoard.vue
+++ src/components/NoticeBoard.vue
@@ -10,19 +10,15 @@
                     <h6>Inbox</h6>
                 </div>
 
-                     <div class="inbox-section--content--according">
-                         <div class="card" v-for="index in 5" :key="index">
-                             <div class="inbox-click" @click="changeToggle(index)">Click me</div>
-                             <i class="fas fa-envelope mail-icon"></i>
-                            <p :class="{ peraToggle: show !== index }">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus 
-                                venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum 
-                                facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis 
-                                scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis eu volutpat odio facilisis mauris 
-                                sit amet massa vitae tortor condimentum lacinia quis vel eros donec ac odio tempor orci dapibus ultrices in iaculis nunc sed augue lacus
-                            </p>
-                         </div>
-                     </div>
+                <div class="inbox-section--content--according">
+                    <div class="card" v-for="(item, index) in notices" :key="index">
+                        <div class="inbox-click" @click="changeToggle(index)">Click me</div>
+                        <i class="fas fa-envelope mail-icon"></i>
+                        <p :class="{ peraToggle: show !== index }">
+                            {{ item['description'] }}
+                        </p>
+                    </div>
+                </div>
 
 
 
@@ -68,89 +64,89 @@
     </section>
 
     <!-- event results -->
-    <section class="event-results">
-        <div class="container">
-                <div class="event-results--img text-center">
-                    <img src="../assets/img/pubg.png" alt="pubg">
-                 </div>
-                 <!-- event-results--content -->
-                 <div class="event-results--content">
-                        <div class="event-results--content--heading">
-                        <h2>ROYAL PASS WINNERS</h2>
-                    </div>
-                    <div class="event-results--content--row">
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                    </div>
-                 </div>
-                  <!-- event-results--content -->
-                 <div class="event-results--content">
-                        <div class="event-results--content--heading">
-                        <h2>MONTHLY TOP BUYERS</h2>
-                    </div>
-                    <div class="event-results--content--row">
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                          <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                    </div>
-                 </div>
-                  <!-- event-results--content -->
-                 <div class="event-results--content">
-                        <div class="event-results--content--heading">
-                        <h2>MONTHLY TOP SELLERS</h2>
-                    </div>
-                    <div class="event-results--content--row">
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                        <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                          <div class="event-results--content--row--box">
-                            <img src="../assets/img/sss.jpg" alt="player">
-                            <p>Arlene McCoy</p>
-                        </div>
-                    </div>
-                 </div>
-        </div>
-    </section>
+<!--    <section class="event-results">-->
+<!--        <div class="container">-->
+<!--                <div class="event-results&#45;&#45;img text-center">-->
+<!--                    <img src="../assets/img/pubg.png" alt="pubg">-->
+<!--                 </div>-->
+<!--                 &lt;!&ndash; event-results&#45;&#45;content &ndash;&gt;-->
+<!--                 <div class="event-results&#45;&#45;content">-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;heading">-->
+<!--                        <h2>ROYAL PASS WINNERS</h2>-->
+<!--                    </div>-->
+<!--                    <div class="event-results&#45;&#45;content&#45;&#45;row">-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                 </div>-->
+<!--                  &lt;!&ndash; event-results&#45;&#45;content &ndash;&gt;-->
+<!--                 <div class="event-results&#45;&#45;content">-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;heading">-->
+<!--                        <h2>MONTHLY TOP BUYERS</h2>-->
+<!--                    </div>-->
+<!--                    <div class="event-results&#45;&#45;content&#45;&#45;row">-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                          <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                 </div>-->
+<!--                  &lt;!&ndash; event-results&#45;&#45;content &ndash;&gt;-->
+<!--                 <div class="event-results&#45;&#45;content">-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;heading">-->
+<!--                        <h2>MONTHLY TOP SELLERS</h2>-->
+<!--                    </div>-->
+<!--                    <div class="event-results&#45;&#45;content&#45;&#45;row">-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                        <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                          <div class="event-results&#45;&#45;content&#45;&#45;row&#45;&#45;box">-->
+<!--                            <img src="../assets/img/sss.jpg" alt="player">-->
+<!--                            <p>Arlene McCoy</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                 </div>-->
+<!--        </div>-->
+<!--    </section>-->
        
 
 
@@ -165,7 +161,8 @@
             return {
                 user: {},
                 isToggle: false,
-                show: -1
+                show: -1,
+                notices: {}
             }
         },
         methods: {
@@ -186,6 +183,14 @@
         created() {
             this.user = this.$store.state.user
             console.log(this.user, 'user');
+
+            this.$api.get('notice').then(response =>
+            {
+                console.log("notice");
+                console.log(response.data.data);
+                this.notices = response.data.data;
+
+            });
         }
     }
 

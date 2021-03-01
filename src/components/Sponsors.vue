2529156c53d53425bb6fd4ddf8850f3e8131c38b
MD Minhaj <minhaj372@gmail.com>
2021-03-01 14:22:20 +0600

    Sponsors Page design

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,85 +1,10 @@
 <template>
     <div>
-        <!-- support-section -->
-        <section class="title-section">
-            <div class="contact-title text-center">
-
-                <div class="gamehub-input-group--content">
-                    <div class="search-input-design support-input-design">
-                        <input type="search" class="" placeholder="How Can We Help?">
-                    </div>
-                    <button class="btn gamehub-search-btn support-search-btn" type="search">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-
-            </div>
-        </section>
-        <!-- support faq -->
-        <section class="support-faq">
+        <!-- sponsors-section -->
+        <section class="sponsors-section">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-10 mx-auto">
-                        <div class="text-center support-sign-in">
-                            <a href="#">SIGN IN FOR CUSTOMISED SUPPORT</a>
-                        </div>
-                        <div class="text-center">
-                            <h6>Sponsors</h6>
-                        </div>
-                        <!-- collapse -->
-                            <div class="accordion" id="accordionExample">
-                                <div class="card">
-                                    <div class="card-header" id="headingOne">
-                                    <h2 class="mb-0">
-                                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Collapsible Group Item #1
-                                        </button>
-                                    </h2>
-                                    </div>
-
-                                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                                    <div class="card-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                    </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-header" id="headingTwo">
-                                    <h2 class="mb-0">
-                                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Collapsible Group Item #2
-                                        </button>
-                                    </h2>
-                                    </div>
-                                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                                    <div class="card-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                    </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-header" id="headingThree">
-                                    <h2 class="mb-0">
-                                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Collapsible Group Item #3
-                                        </button>
-                                    </h2>
-                                    </div>
-                                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                                    <div class="card-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-                        <!-- end collapse -->
-                        <div class="text-center support-bottom">
-                            <h6>STILL LOOKING FOR HELP?</h6>
-                            <p>Opening a support case is easy. Email us your issue or chat with an expert</p>
-                            <a href="#">SIGN IN TO OPEN A SUPPORT CASE</a>
-                        </div>
-
-                    </div>
+                <div class="sponsors-section--content">
+                    <h2>coming soon....</h2>
                 </div>
             </div>
         </section>

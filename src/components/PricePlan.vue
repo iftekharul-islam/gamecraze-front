a767f7c2f8c9b46fc839ef14293c296b4b4ab9b4
MD Minhaj <minhaj372@gmail.com>
2021-04-07 15:01:15 +0600

    Elite and Rookie separately design

--- src/components/PricePlan.vue
+++ src/components/PricePlan.vue
@@ -9,30 +9,43 @@
                             </div>
                          <div class=" pricing-plan--card--main">
                             <div class="pricing-plan--card--price text-center">
-                                <h2 class="text-secondery mb-2">3000 taka</h2>
-                                <span class="gil-medium f-s-20">(Refundable)</span>
+                                <h2 class="text-secondery mb-2">BDT 2999</h2>
+                                <span class="gil-medium f-s-20">(100% Refundable)</span>
                             </div>
                             <div class="pricing-plan--card--list">
                                 <div class="pricing-plan--card--list--item">
                                     <span class="box eligible"></span>
-                                    <span>Demo text here</span>
+                                    <span>Rent both physical & digital games</span>
                                 </div>
                                 <div class="pricing-plan--card--list--item">
                                     <span class="box eligible"></span>
-                                    <span>Demo text here</span>
+                                    <span>Rent Unlimited physical & digital games</span>
                                 </div>
                                 <div class="pricing-plan--card--list--item">
                                     <span class="box eligible"></span>
-                                    <span>Demo text here</span>
+                                    <span>Can rent 2 Games at a time </span>
                                 </div>
                                 <div class="pricing-plan--card--list--item">
                                     <span class="box eligible"></span>
-                                    <span>Demo text here</span>
+                                    <span>Free delivery after 3rd rent </span>
                                 </div>
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box eligible"></span>
+                                    <span>Quick payment for money earned from lending </span>
+                                </div>
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box eligible"></span>
+                                    <span>24/7 support </span>
+                                </div>
+
                             </div>
                             <div class="pricing-plan--card--description">
                                 <p class="f-s-14 mb-0">
-                                    The Elite members need to pay 2999 BDT which allows them to rent games for three months. The amount is refundable unless the member breaks any rules or involves themselves in any fraudulent activity. This type of membership will allow the player to rent unlimited games during the period but they can not possess more than two games at the given period. The elite members will be allowed to rent both physical and digital games.
+                                    The Elite members need to pay 2999 BDT which allows them to rent games for three months. 
+                                    The amount is refundable unless the member breaks any rules or involves themself in any 
+                                    fraudulent activity. This type of membership will allow the player to rent unlimited games 
+                                    during the period but they can not possess more than two games at the given period. 
+                                    The elite members will be allowed to rent both physical and digital games
                                 </p>
                             </div>
                             <div class="pricing-plan--card--btn">
@@ -52,24 +65,45 @@
                             <div class="pricing-plan--card--list">
                                 <div class="pricing-plan--card--list--item">
                                     <span class="box eligible"></span>
-                                    <span>Demo text here</span>
+                                    <span>Rent only digital games </span>
+                                </div>
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box eligible"></span>
+                                    <span>Rent Unlimited digital games</span>
+                                </div>
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box eligible"></span>
+                                    <span>Can rent 2 Games at a time</span>
+                                </div>
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box eligible"></span>
+                                    <span>Quick payment for money earned from lending</span>
+                                </div>
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box eligible"></span>
+                                    <span>Official support (10 am to 7 pm)</span>
+                                </div>
+                                <!-- Not for free user -->
+                                <div class="pricing-plan--card--list--item">
+                                    <span class="box not-eligible"></span>
+                                    <span>Rent both physical & digital games</span>
+                                </div>
+                                 <div class="pricing-plan--card--list--item">
+                                    <span class="box not-eligible"></span>
+                                    <span>Free delivery after 3rd rent </span>
                                 </div>
                                 <div class="pricing-plan--card--list--item">
                                     <span class="box not-eligible"></span>
-                                    <span>Demo text here</span>
+                                    <span>24/7 support </span>
                                 </div>
-                                <div class="pricing-plan--card--list--item">
-                                    <span class="box not-eligible"></span>
-                                    <span>Demo text here</span>
-                                </div>
-                                <div class="pricing-plan--card--list--item">
-                                    <span class="box not-eligible"></span>
-                                    <span>Demo text here</span>
-                                </div>
+
                             </div>
                             <div class="pricing-plan--card--description">
                                 <p class="f-s-14 mb-0">
-                                    The Rookie membership is free. This type of membership allow the player to rent unlimited digital games but they can not possess more than two games at any given period. The rookie members will be able to rent only digital games. The digital version of the games will be delivered to gamers via email. If the members want to rent physical games they need to upgrade to elite membership.
+                                   The Rookie membership will be free. This type of membership will allow the user 
+                                   to rent unlimited digital games but they can not possess more than two games at 
+                                   any given period. The rookie members will be able to rent only digital games. 
+                                   The digital version of the games will be delivered to gamers via email. 
                                 </p>
                             </div>
                             <div class="pricing-plan--card--btn">

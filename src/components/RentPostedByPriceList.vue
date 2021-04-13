0724fe5860ab7696d9c936d93d633a8a7d506040
iftekharul <iftekharul@augnitive.com>
2021-04-13 17:35:20 +0600

    Rent price list page design

--- src/components/RentPostedByPriceList.vue
+++ src/components/RentPostedByPriceList.vue
@@ -17,22 +17,28 @@
                         </thead>
 
                         <tbody>
-                        <tr v-for="(rent, index) in rentPosts" @click="$router.push('/login')" :key="index">
-                            <td scope="col" v-if="rent.user.data.image"><img :src="rent.user.data.image" alt="renter">{{ rent.user.data.name }}</td>
-                            <td scope="col" v-else><img width="80px" v-if="rent.user.data.gender === 'Male'" src="../assets/img/male.png" alt="renter"><img width="80px" v-else-if="rent.user.data.gender === 'Female'" src="../assets/img/female.png" alt="renter"><img v-else src="../assets/img/avatar.png" width="80px" alt="renter">{{ rent.user.data.name }}</td>
-                            <td scope="col" v-if="rent.rented_user_id !== null"><span>Rented</span></td>
-                            <td scope="col" v-else>Available</td>
-                            <!--                            <td scope="col" v-if="rent.checkpoint">{{ rent.checkpoint.data.area.data.name }}</td>-->
-                            <td scope="col" v-else>Not Set</td>
-                            <td scope="col" v-if="rent.disk_type == 1">Physical Disk</td>
-                            <td scope="col" v-else>Digital Disk</td>
-                            <td scope="col">{{ formattedDate(rent.availability_from_date) }}</td>
-                            <td scope="col"><span>{{ rent.max_number_of_week}} week(s)</span></td>
-                            <td>
-                                <del class="mr-4"><span> ৳</span> {{ rent.price_combination.regular_price}}</del>
-                                <span class="new-price"><span>৳</span> {{ rent.price_combination.discount_price}}</span>
-                            </td>
-                        </tr>
+                            <tr v-for="(rent, index) in rentPosts" :key="index" @click="$router.push('/login')" :class="{disablePost: rent.rented_user_id !== null}">
+                                <td scope="col" v-if="rent.user.data.image">
+                                    <img :src="rent.user.data.image" alt="renter">{{ rent.user.data.name }}
+                                </td>
+                                <td scope="col" v-else>
+                                    <img width="80px" v-if="rent.user.data.gender === 'Male'" src="../assets/img/male.png" alt="renter">
+                                    <img width="80px" v-else-if="rent.user.data.gender === 'Female'" src="../assets/img/female.png" alt="renter">
+                                    <img v-else src="../assets/img/avatar.png" width="80px" alt="renter">{{ rent.user.data.name }}
+                                </td>
+                                <td scope="col" v-if="rent.rented_user_id !== null"><span>Rented</span></td>
+                                <td scope="col" v-else>Available</td>
+                                <!--                            <td scope="col" v-if="rent.checkpoint">{{ rent.checkpoint.data.area.data.name }}</td>-->
+                                <td scope="col" v-else>Not Set</td>
+                                <td scope="col" v-if="rent.disk_type == 1">Physical Disk</td>
+                                <td scope="col" v-else>Digital Disk</td>
+                                <td scope="col">{{ formattedDate(rent.availability_from_date) }}</td>
+                                <td scope="col"><span>{{ rent.max_number_of_week}} week(s)</span></td>
+                                <td>
+                                    <del class="mr-4"><span> ৳</span> {{ rent.price_combination.regular_price}}</del>
+                                    <span class="new-price"><span>৳</span> {{ rent.price_combination.discount_price}}</span>
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>

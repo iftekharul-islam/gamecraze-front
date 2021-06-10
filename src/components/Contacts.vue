4fd1b593decbdfdd1fdc8f030b8ee7efd873e498
MD Minhaj <minhaj372@gmail.com>
2021-06-10 15:46:23 +0600

    New privacy policy add and google map add in contact us page

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -12,9 +12,10 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="map">
-                            <a href="https://goo.gl/maps/K1gTqktfgaojX44o9" target="_blank" title="View location in google map">
+                            <!-- <a href="https://goo.gl/maps/K1gTqktfgaojX44o9" target="_blank" title="View location in google map">
                                 <img src="../assets/img/map.png" alt="map" class="img-fluid">
-                            </a>
+                            </a> -->
+                            <iframe class="w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.4110149219805!2d90.3630115153858!3d23.8395344912814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c1c583da4837%3A0x3b78f9f59ec865b6!2sAugnitive!5e0!3m2!1sen!2sbd!4v1623317482971!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                         </div>
                     </div>
                     <div class="col-md-6 mt-4 mt-md-0">

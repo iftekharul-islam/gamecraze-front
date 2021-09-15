de46f308f11cd3f4c874ce94f9ce4d4dc5088061
iftekharul <iftekharul@augnitive.com>
2021-09-15 13:25:46 +0600

    Code refactore

--- src/components/CreateSellPost.vue
+++ src/components/CreateSellPost.vue
@@ -186,7 +186,7 @@
                 <div class="group mb-a-6 mt-a-6">
                   <label class="mb-3 w-100">{{ $t('upload_screenshots', $store.state.locale) }}</label>
                   <div class="post-rent--form-group--input wizard__body__step">
-                     <UploadImages class="w-100 p-0 bg-transparent border-0" :max="4" maxError="Max image upload limit is 4" @changed="uploadScreenshots"/>
+                     <UploadImages class="w-100 p-0 bg-transparent border-0" :max="4" maxError="Max image upload limit is 4" @change="uploadScreenshots" @changed="uploadScreenshots"/>
                   </div>
                  
                     <span class="text-step-error mt-2 d-inline-block" v-if="screenshotsError"> {{ $t('please_upload_screenshots', $store.state.locale) }}</span>
@@ -493,7 +493,6 @@
         }, this.mime_type)
       },
       uploadScreenshots(files) {
-        console.log('hello')
         this.screenshotsError = false;
         if (files.length === 0) {
           this.postImages = [];
